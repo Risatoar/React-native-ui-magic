3a812b08fe511da0b054cfbbafdb81fbdf442640
xuxiang <xuxiang@tuya.com>
2018-06-29 17:47:10 +0800

    some fix

--- src/divider/index.js
+++ src/divider/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { View, StyleSheet, ViewPropTypes, ColorPropType } from 'react-native';
+import PropTypes from 'prop-types';
 
 const hairlineWidth = StyleSheet.hairlineWidth;
 
